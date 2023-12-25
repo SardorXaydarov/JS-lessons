153be7bde022f77e5b680f730a31f7a969e4a788
Xaydarov Sardor <150023132+SardorXaydarov@users.noreply.github.com>
2023-12-25 14:32:00 +0000

    block-scope added

---
+++ block-scope/index.js
@@ -0,0 +1,21 @@
+// Global scope
+let ism= 'Sardor'
+ism='Oybek'
+if(true){
+    let ism='Sardor'
+    console.log(ism);
+}
+console.log(ism);
+let age =16
+age=18
+console.log(age)
+if(true){
+    let age=19
+    console.log(age)
+    if(true){
+        console.log('Ikkinchi else if ichi:', age)
+    }
+}
+if(true){
+    console.log('Tashqi else if ichi:' ,age);
+}
